d487fc1b6e696f75696c86a1cae6e564229b0479
Damir Cosic <dcosic@urban.org>
2023-10-10 16:53:02 -0400

    Moves NLSY data to Box.

--- NLSY/make_nlsy.R
+++ NLSY/make_nlsy.R
@@ -1,14 +1,27 @@
 
+# Execute the script provided by NLSY that reads the data and 
+# save them in an RDS file.
 
 library(tidyverse)
 
-setwd(paste(here::here(), "NLSY", sep="/"))
+# Get the NLSY data path
+source('common.R')
+
+# The NLSY script has to be executed in the data folder
+setwd(nlsy_data_dir)
 source("College-finance2.R")
+
+# Frame with weights
+wtdf = read_table(
+  'customweight_nlsy97_651b190a18af1d9d269.dat',
+  col_names = c('PUBID_1997','wt'),
+  col_types = 'id'
+) |>
+  mutate(wt=wt/100)
+
+categories = left_join(categories, wtdf, by='PUBID_1997')
+
 setwd(here::here())
-
-rm(new_data)
-
-nlsydf = categories
-rm(categories)
-saveRDS(nlsydf, "NLSY/NLSY-college-finance2.rds")
+saveRDS(categories, "NLSY/NLSY-college-finance.rds")
+rm(list=c('new_data', 'categories', 'wtdf', 'qnames', 'varlabels', 'vallabels', 'vallabels_continuous'))
 
---
+++ common.R
@@ -0,0 +1,3 @@
+
+
+nlsy_data_dir = '~/../Box/Baby Bonds Microsimulation Modeling-Rockefeller/Data/NLSY'
