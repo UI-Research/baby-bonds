91bfc8edec80d11dd6bacce1db4165447f2cd23c
Damir Cosic <dcosic@urban.org>
2023-10-11 09:57:57 -0400

    Adds username in common.R.

--- common.R
+++ common.R
@@ -1,3 +1,11 @@
 
+# This file defines common variables
+
+username = tolower(Sys.getenv('USERNAME'))
 
 nlsy_data_dir = '~/../Box/Baby Bonds Microsimulation Modeling-Rockefeller/Data/NLSY'
+
+# If the Box path is different on your computer, define it here
+if(username == 'dcosic') {
+  nlsy_data_dir = '~/../Box/Baby Bonds Microsimulation Modeling-Rockefeller/Data/NLSY'
+}
