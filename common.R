d035d8584ebb205c8a789fc87888609528e39607
storresrod <69353664+storresrod@users.noreply.github.com>
2023-11-07 10:25:28 -0600

    First draft of the nlsy_impute.R script
    First attempt at issue #1, imputing values for missing wealth and income using multiple imputation

--- common.R
+++ common.R
@@ -6,6 +6,6 @@ username = tolower(Sys.getenv('USERNAME'))
 nlsy_data_dir = '~/../Box/Baby Bonds Microsimulation Modeling-Rockefeller/Data/NLSY'
 
 # If the Box path is different on your computer, define it here
-if(username == 'dcosic') {
+if(username == 'storresrod') {
   nlsy_data_dir = '~/../Box/Baby Bonds Microsimulation Modeling-Rockefeller/Data/NLSY'
 }
