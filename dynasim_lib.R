55218e1c9a186ff6b869f80d46c6b1c62efdacbf
Damir Cosic <damir.cosic@gmail.com>
2023-12-02 15:49:51 -0500

    Initial commit.

---
+++ dynasim_lib.R
@@ -0,0 +1,47 @@
+
+
+#' Writes coefficients of a list of models into a DYNASIM coefficient file
+#'
+#' @param models - list of models
+#' @param filename - path of output file
+#' @param varnames - a list of variable names, up to 8 characters long
+#' @param modnames - model names, up to 10 characters long
+#' @param description - a short description
+dyn_write_coef_file = function(models, filename, varnames, modnames, description)
+{
+    # Number of variables and models
+    nv = dim(df)[1]
+    ng = length(models)
+
+    # Create a dataframe with models' coefficients
+    df = as_tibble(map(models, coef)) |>
+        mutate(
+            N=row_number(),
+            varname=str_pad(varnames, width=8, side='right'),
+            varname=sQuote(varname, q=FALSE),
+            across(-c(N,varname), ~format(.x, width=10, digits=6))
+        ) |>
+        relocate(N, varname)
+
+    # File header
+    #   - lines starting with ';' are comments
+    #   - lines between $COEFINFO and $END are paramaters
+    header = paste0(
+        ';\n',
+        '; ', gsub('\\.CSV', '', toupper(basename(filename))), ' - ', description, '\n',
+        '; ', today(), '\n',
+        ';\n',
+        ';\n',
+        ';', paste(rep(" ", 11), collapse=""),
+        paste(format(modnames, width=11, justify='right'), collapse=""), '\n',
+        paste0(';', paste(rep(rep('=',11), ng+1), collapse=""),'\n'),
+        '$COEFINFO\n',
+        ' NV = ', format(nv, width=4), ',\n',
+        ' NG = ', format(ng, width=4), '\n',
+        '$END\n'
+    )
+
+    # Write it to file
+    cat(header, file=filename)
+    write_csv(df, filename, append=TRUE)
+}
